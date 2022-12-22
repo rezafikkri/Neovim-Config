13ce34cf07e229d1c024dd62bd35bf13cb27f51b
rezafikkri <fikkri.reza@gmail.com>
2022-12-22 08:46:43 +0700

    refactor: nvim tree config

--- lua/nvimtree.lua
+++ lua/nvimtree.lua
@@ -4,11 +4,16 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
 -- OR setup with some options
 require("nvim-tree").setup({
+  open_on_tab = true,
+  hijack_cursor = true,
+  update_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  }, 
   git = {
     ignore = false,
   }
