aa290e6675cb373d5c0568b34773dbfe473b518d
rezafikkri <fikkri.reza@gmail.com>
2022-12-25 12:51:32 +0700

    refactor: remove hiject cursor in nvim tree config

--- lua/nvimtree.lua
+++ lua/nvimtree.lua
@@ -7,7 +7,6 @@ vim.g.loaded_netrwPlugin = 1
 -- OR setup with some options
 require("nvim-tree").setup({
   open_on_tab = true,
-  hijack_cursor = true,
   update_cwd = true,
   respect_buf_cwd = true,
   update_focused_file = {
